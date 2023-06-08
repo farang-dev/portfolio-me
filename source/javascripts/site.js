35805a9883022f3911d5c13e8bd1bb298d92851e
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-06-09 00:10:49 +0900

    added smooth scrolling when the link is clicked

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,12 @@
 // This is where it all goes :)
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
